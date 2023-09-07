7dd48a0ad5d76c9ce82def7fdfa159c4279e1a53
ccadran <clariocadran33@gmail.com>
2023-09-07 09:56:01 +0200

    create projects page

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProjectHome from "../components/ProjectHome";
+
+export default function ProjectsPage() {
+  return (
+    <section>
+      <h1>Projects</h1>
+      <p>ProjectsPage</p>
+      <ProjectHome />
+    </section>
+  );
+}
