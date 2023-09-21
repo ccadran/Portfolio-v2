97256c38164afc38b983b5026196eab8dedcd124
ccadran <clariocadran33@gmail.com>
2023-09-21 10:19:04 +0200

    add tilt effects

--- src/app/components/ListItemProjects.tsx
+++ src/app/components/ListItemProjects.tsx
@@ -21,36 +21,38 @@ export default function ListItemProjects({ project }: Props) {
   } = project;
 
   // Sélectionnez l'élément .project
-  const projectElement = document.querySelector(".project") as HTMLElement;
+  useEffect(() => {
+    const projectElement = document.querySelectorAll(".project");
 
-  // Vérifiez si l'élément existe avant d'initialiser VanillaTilt
-  if (projectElement) {
-    // Initialisez VanillaTilt avec l'élément .project
-    VanillaTilt.init(projectElement, {
-      max: 5,
-      speed: 10,
-      glare: true,
-      "max-glare": 50,
-    });
-  }
+    // Vérifiez si l'élément existe avant d'initialiser VanillaTilt
+    if (projectElement) {
+      // Initialisez VanillaTilt avec l'élément .project
+      VanillaTilt.init(projectElement, {
+        max: 5,
+        speed: 10,
+        glare: true,
+        "max-glare": 20,
+      });
+    }
+  }, []);
 
   return (
-    <Link href={`/projects/${id}`}>
-      <div className="project">
-        <h3>{title}</h3>
-        <div className="content">
-          <div className="img-container">
-            <img src={mainImg} alt="" />
-          </div>
-          <div className="left">
-            <p>{shortDescription}</p>
-            <h4>
-              {projectType} * {technologies} * {date.slice(6, 10)}
-            </h4>
-            <Button link={`/projects/${id}`} text="Voir le projet" />
-          </div>
+    // <Link href={`/projects/${id}`}>
+    <div className="project">
+      <h3>{title}</h3>
+      <div className="content">
+        <div className="img-container">
+          <img src={mainImg} alt="" />
+        </div>
+        <div className="left">
+          <p>{shortDescription}</p>
+          <h4>
+            {projectType} * {technologies} * {date.slice(6, 10)}
+          </h4>
+          <Button link={`/projects/${id}`} text="Voir le projet" />
         </div>
       </div>
-    </Link>
+    </div>
+    // </Link>
   );
 }
