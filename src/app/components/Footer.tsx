e3b7a394da78d9df3ac1f2165a1197e568255fad
ccadran <clariocadran33@gmail.com>
2023-10-06 11:02:07 +0200

    change github white icon2

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -42,7 +42,7 @@ export default function Footer() {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <img src="/assets/logos/Github-white.svg" alt="" />
+                <img src="/assets/logos/Github-black.svg" alt="" />
               </Link>
             </ul>
           </div>
