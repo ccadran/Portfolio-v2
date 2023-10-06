7c2df1301355c578749f4f4502129f595df0c851
ccadran <clariocadran33@gmail.com>
2023-10-06 10:24:40 +0200

    change github icon

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -42,7 +42,7 @@ export default function Footer() {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <img src="/assets/logos/Linkedin-black.svg" alt="" />
+                <img src="/assets/logos/Github-black.svg" alt="" />
               </Link>
             </ul>
           </div>
