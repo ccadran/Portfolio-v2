cfe81de02bc820675af21b9cee848350fde728a2
ccadran <clariocadran33@gmail.com>
2023-10-15 13:25:48 +0200

    add transition on project page w/ framer-motion

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -4,6 +4,7 @@ import { notFound } from "next/navigation";
 import Link from "next/link";
 import Button from "@/app/components/Button";
 import "../../../style/pages/_project.scss";
+import PageTransition from "@/app/components/PageTransition";
 
 export function generateStaticParams() {
   const projects = getSortedProjectData();
@@ -65,47 +66,49 @@ export default async function Project({
   const logosImgs = await getLogosImgs(tools.logos);
 
   return (
-    <main>
-      <section className="hero-project">
-        <h1>{title}</h1>
-        <div className="presentation">
-          <h4>
-            {projectType} * {technologies} * {date.slice(6, 10)}
-          </h4>
-          <p>{shortDescription}</p>
-          <Button link={url} text="Voir le projet" newTab={true} />
+    <PageTransition>
+      <main>
+        <section className="hero-project">
+          <h1>{title}</h1>
+          <div className="presentation">
+            <h4>
+              {projectType} * {technologies} * {date.slice(6, 10)}
+            </h4>
+            <p>{shortDescription}</p>
+            <Button link={url} text="Voir le projet" newTab={true} />
+          </div>
+        </section>
+        <div className="cover">
+          <img src={mainImg} alt="" />
         </div>
-      </section>
-      <div className="cover">
-        <img src={mainImg} alt="" />
-      </div>
-      <section className="project-description">
-        <div className="txt-project">
-          <h4>{title}</h4>
-          <p>{longDescription}</p>
-        </div>
-        <div className="img-project">
-          {illustrationsImgs.map((illustrationImg) => (
-            <div className="img-container" key={illustrationImg}>
-              <img src={illustrationImg} alt="" />
-            </div>
-          ))}
-        </div>
-      </section>
-      <section className="tools">
-        <h2>Tools</h2>
-        <div className="tools-container">
-          <ul>
-            {logosImgs.map((logoImg) => (
-              <li key={logoImg}>
-                <img src={logoImg} alt="" width="50px" height="50px" />
-              </li>
+        <section className="project-description">
+          <div className="txt-project">
+            <h4>{title}</h4>
+            <p>{longDescription}</p>
+          </div>
+          <div className="img-project">
+            {illustrationsImgs.map((illustrationImg) => (
+              <div className="img-container" key={illustrationImg}>
+                <img src={illustrationImg} alt="" />
+              </div>
             ))}
-          </ul>
-          <p>{tools.toolsDescription}</p>
-          <Button link={tools.urlGithub} text="Voir le code" newTab={true} />
-        </div>
-      </section>
-    </main>
+          </div>
+        </section>
+        <section className="tools">
+          <h2>Tools</h2>
+          <div className="tools-container">
+            <ul>
+              {logosImgs.map((logoImg) => (
+                <li key={logoImg}>
+                  <img src={logoImg} alt="" width="50px" height="50px" />
+                </li>
+              ))}
+            </ul>
+            <p>{tools.toolsDescription}</p>
+            <Button link={tools.urlGithub} text="Voir le code" newTab={true} />
+          </div>
+        </section>
+      </main>
+    </PageTransition>
   );
 }
