28f55b3215c56114e3253a5757ce736de371829f
ccadran <clariocadran33@gmail.com>
2023-10-30 12:46:38 +0100

    test

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -22,6 +22,20 @@ const menuSlide = {
   },
 };
 
+// export const menuSlide = {
+//   initial: {
+//     x: "calc(100% + 100px)",
+//   },
+//   enter: {
+//     x: "0%",
+//     transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1] },
+//   },
+//   exit: {
+//     x: "calc(100% + 100px)",
+//     transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1] },
+//   },
+// };
+
 export default function Navbar() {
   const pathname = usePathname();
   const [isResponsiveNavOpen, setResponsiveNavOpen] = useState(false);
