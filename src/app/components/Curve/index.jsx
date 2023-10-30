9ee61ad1800dba34ad6b8ef7667501d119e15b67
ccadran <clariocadran33@gmail.com>
2023-10-30 13:50:00 +0100

    make curve anim longer

--- src/app/components/Curve/index.jsx
+++ src/app/components/Curve/index.jsx
@@ -19,11 +19,11 @@ export default function Curve() {
     },
     enter: {
       d: targetPath,
-      transition: { duration: 1, ease: [0.76, 0, 0.24, 1] },
+      transition: { duration: 1.4, ease: [0.76, 0, 0.24, 1] },
     },
     exit: {
       d: initialPath,
-      transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1] },
+      transition: { duration: 1.2, ease: [0.76, 0, 0.24, 1] },
     },
   };
 
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -14,27 +14,13 @@ const menuSlide = {
   },
   enter: {
     transform: "translateX(0%)",
-    transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1] },
+    transition: { duration: 1.2, ease: [0.76, 0, 0.24, 1] },
   },
   exit: {
     transform: "translateX(calc(100% + 100px))",
-    transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1] },
+    transition: { duration: 1.2, ease: [0.76, 0, 0.24, 1] },
   },
 };
-
-// export const menuSlide = {
-//   initial: {
-//     x: "calc(100% + 100px)",
-//   },
-//   enter: {
-//     x: "0%",
-//     transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1] },
-//   },
-//   exit: {
-//     x: "calc(100% + 100px)",
-//     transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1] },
-//   },
-// };
 
 export default function Navbar() {
   const pathname = usePathname();
