998086183a40386ec3fce59a074b073defc0e6f8
ccadran <clariocadran33@gmail.com>
2023-10-30 17:31:38 +0100

    test

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -150,7 +150,7 @@ export default function Navbar() {
                   );
                 })}
               </ul>
-              {/* <Curve /> */}
+              <Curve />
             </motion.div>
           )}
         </AnimatePresence>
